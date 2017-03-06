0b77e86276b2690d8e610ab1436f7adfa6e20659
Stig Telfer <stig.openstack@telfer.org>
2017-03-06 08:54:56 +0000

    RADOS bench setup script tweak

--- 4-rados-aggregate-single/radosbench-setup.sh
+++ 4-rados-aggregate-single/radosbench-setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -x
+# This script is run on the rados bench client.
+# It must have passwordless ssh access to root on the target OSD server.
+
+osd_server=softiron-osd3
 
 # Run the test setup on all pools first, to drive data through the cache
 # ... and out the other side
@@ -10,7 +14,7 @@ done
 # Read-back phase
 for i in $(seq 2 2 12 )
 do
-    coproc ssh softiron-osd3 iostat 1 > iostat-${i}.dat
+    coproc ssh $osd_server iostat 1 > iostat-${i}.dat
     sleep 2
 
     rados bench -k /etc/ceph/ceph.client.admin.keyring -t 64 30 -p stig_osd3_$i seq | tee stig_osd3_${i}.radosbench
