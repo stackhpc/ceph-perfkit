dd4ed82dac58767aa34dabf288b4187b6abcbefe
Stig Telfer <stig@stackhpc.com>
2017-06-09 17:34:06 +0100

    Updated test config for test 3. (TCP_SENDFILE iperf).

--- 3-iperf-aggregate-sendfile/iperf-client.sh
+++ 3-iperf-aggregate-sendfile/iperf-client.sh
@@ -5,17 +5,15 @@ target=$1
 # Generate a lookup table of HDDs to include, in a sort-of
 # alternating order between SATA controllers
 i=0
-for hdd in sdb sdj sdc sdk sdd sdl sde sdm sdf sdn sdg sdh
+for dev in sdd sdj sde sdk sdf sdl sdg sdm sdh sdn
 do 
-    osd[$i]=$hdd
+    osd[$i]=$dev
     i=$(($i + 1))
 done
-ssd=sdi
 
-for ((count=0; $count <= 12; count=$count+2))
+for ((count=1; $count <= 10; count=$count+1))
 do
     echo "Testing $count OSDs..."
-    iperf3 -f M -c $target -p 5200 -F /dev/$ssd -Z -T $ssd -J > iperf3-$count-$ssd.json &
     for ((j=0; $j < $count; j=$j+1))
     do
         hdd=${osd[$j]}
--- 3-iperf-aggregate-sendfile/iperf-munge.sh
+++ 3-iperf-aggregate-sendfile/iperf-munge.sh
@@ -2,14 +2,14 @@
 # This script transforms the JSON output from a set of test runs
 # into something that gnuplot can process.
 
-for dev in sd{b..n}
+for dev in sd{{d..h},{j..n}}
 do
     > iperf3-${dev}.dat
 done
 
-for ((count=0; $count <= 12; count=$count+2))
+for ((count=1; $count <= 10; count=$count+1))
 do
-    for dev in sd{b..n}
+    for dev in sd{{d..h},{j..n}}
     do
         dev_data=iperf3-$count-$dev.json 
         dev_bwidth=0
